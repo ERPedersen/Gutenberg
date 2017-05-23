86bc42e876f0a5e0f4fcf47a84f1a62ec534f8e8
Ebbe Nielsen <Ebski91@gmail.com>
2017-05-23 18:44:23 +0200

    #25 Refactoring of javadoc in IDBConnectorMySQL

--- src/main/java/main/util/IDBConnectorMySQL.java
+++ src/main/java/main/util/IDBConnectorMySQL.java
@@ -3,14 +3,20 @@ package main.util;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-/***
- * Interface for the DBConnectorMySQL class.
- * 
- * @author Dennis
- */
 public interface IDBConnectorMySQL {
 
+	/**
+	 * Checks if the connection is null or closed, and returns either a new or the old connection.
+	 *
+	 * @return The connection to the MySQL database.
+	 * @throws SQLException If there is a problem connecting to the database.
+	 * @throws ClassNotFoundException If the JDBC driver was not found.
+	 */
 	Connection getConnection() throws SQLException, ClassNotFoundException;
+
+	/**
+	 * Checks if the connection is open and closes it.
+	 */
 	void closeConnection();
 
 }
