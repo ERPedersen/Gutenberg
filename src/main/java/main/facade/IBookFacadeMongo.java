1aee6ec703c52f622761f9fcaa0186183368c2ce
Ebbe Nielsen <Ebski91@gmail.com>
2017-05-23 18:39:49 +0200

    #25 Refactoring of javadoc in IBookFacadeMySQL

--- src/main/java/main/facade/IBookFacadeMongo.java
+++ src/main/java/main/facade/IBookFacadeMongo.java
@@ -12,6 +12,8 @@ public interface IBookFacadeMongo {
      *
      * @param latitude The latitude.
      * @param longitude The longitude.
+     * @param radius The radius of the search.
+     * @param limit The limit of Books allowed from the search.
      * @throws BookNotFoundException Is thrown if no books are found.
      */
     public List<Book> getBooksFromLatLong(double latitude, double longitude, int radius, int limit) throws BookNotFoundException;
@@ -20,6 +22,7 @@ public interface IBookFacadeMongo {
      * Returns a List of books an author has written.
      *
      * @param name The name of the author.
+     * @param limit The limit of Books allowed from the search.
      * @return The books the author has written.
      * @throws BookNotFoundException Is thrown if no books are found.
      */
@@ -29,6 +32,7 @@ public interface IBookFacadeMongo {
      * Returns a List of books where cities mentioned in a book is in.
      *
      * @param title The title of a book.
+     * @param limit The limit of Books allowed from the search.
      * @return The books where the cities are mentioned.
      * @throws BookNotFoundException Is thrown if no books are found.
      */
@@ -38,6 +42,7 @@ public interface IBookFacadeMongo {
      * Returns a List of books which has a specific location somewhere in the text.
      *
      * @param name The name of the location that is mentioned.
+     * @param limit The limit of Books allowed from the search.
      * @return The books where the location is mentioned.
      * @throws BookNotFoundException Is thrown if no books are found.
      */
